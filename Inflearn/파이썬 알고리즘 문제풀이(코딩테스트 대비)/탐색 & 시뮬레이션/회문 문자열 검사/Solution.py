4541c23b88682ad7ff6391986b6e8d83d549461b
Jangoh Jeong <sunjh96@gmail.com>
2022-05-05 04:31:23 +0800

    22/05/05

--- Inflearn/파이썬 알고리즘 문제풀이(코딩테스트 대비)/탐색 & 시뮬레이션/회문 문자열 검사/Solution.py
+++ Inflearn/파이썬 알고리즘 문제풀이(코딩테스트 대비)/탐색 & 시뮬레이션/회문 문자열 검사/Solution.py
@@ -1,9 +1,23 @@
 n = int(input())
 arr = []
+
 for i in range(n):
-    arr.append(str(input()))
-    print(arr.reverse())
-    if arr[i] == arr.reverse():
-        print(f'#{i} YES')
+    string = input()
+    string = string.lower()
+    if string == string[::-1]:
+        print(f'#{i + 1} YES')
     else:
-        print(f'#{i} NO')
+        print(f'#{i + 1} NO')
+
+'''
+for i in range(n):
+    string = input()
+    string = string.lower()
+
+    for j in range(len(string) // 2):
+        if string[j] != string[-1-j]:
+            print("%d NO" %(i + 1))
+            break
+    else:
+        print("%d YES" %(i + 1))
+'''
