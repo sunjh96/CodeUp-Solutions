c7501d1968e46401acfc2957f3c1cc3af241b67d
Jangoh Jeong <sunjh96@gmail.com>
2022-05-05 22:37:15 +0800

    Update Solution.py

---
+++ Inflearn/파이썬 알고리즘 문제풀이(코딩테스트 대비)/탐색 & 시뮬레이션/숫자만 추출/Solution.py
@@ -0,0 +1,28 @@
+string = input()
+arr = []
+ans = 0
+cnt = 0
+
+for i in string:
+    if i.isdecimal():
+        ans = 10 * ans + int(i)
+
+for i in range(1, ans //2 + 1):
+    if ans % i == 0:
+        cnt += 1
+
+print(ans, cnt + 1)
+'''
+for i in string:
+    if i.isalpha() == False:
+        arr.append(i)
+
+ans = "".join(arr)
+ans = int(ans)
+
+for i in range(1, ans // 2 + 1):
+    if ans % i == 0:
+        cnt += 1
+
+print(ans, cnt + 1)
+'''
