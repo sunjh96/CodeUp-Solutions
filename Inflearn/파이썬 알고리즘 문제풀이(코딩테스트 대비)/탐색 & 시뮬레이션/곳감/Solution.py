9678c28a29b37401ac49c1dda8ee651c24535613
Jangoh Jeong <sunjh96@gmail.com>
2022-05-11 21:34:42 +0800

    22/05/11

---
+++ Inflearn/파이썬 알고리즘 문제풀이(코딩테스트 대비)/탐색 & 시뮬레이션/곳감/Solution.py
@@ -0,0 +1,27 @@
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def push(self, val):
+        self.items.append(val)
+
+    def pop(self):
+       try:
+           return self.items.pop()
+       except IndexError:
+           print("Stack is empty")
+
+    def top(self):
+        try:
+            return self.items[-1]
+        except IndexError:
+            print("Stack is empty")
+
+    def __len__(self):
+        return len(self.items)
+
+S = Stack()
+S.push(10)
+S.top()
+print(S.len())
+#print(len(S))
