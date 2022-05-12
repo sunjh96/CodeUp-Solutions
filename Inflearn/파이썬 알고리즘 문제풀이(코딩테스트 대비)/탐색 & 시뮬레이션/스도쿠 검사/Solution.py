87da58c55010df1cc70f8e63244fd1ca5d6b6551
Jangoh Jeong <sunjh96@gmail.com>
2022-05-13 05:32:20 +0800

    22/05/13

--- Inflearn/파이썬 알고리즘 문제풀이(코딩테스트 대비)/탐색 & 시뮬레이션/스도쿠 검사/Solution.py
+++ Inflearn/파이썬 알고리즘 문제풀이(코딩테스트 대비)/탐색 & 시뮬레이션/스도쿠 검사/Solution.py
@@ -4,9 +4,44 @@ arr = [list(map(int, input().split())) for _ in range(n)]
 ans = True
 check = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 temp = []
+s = e = 0
+
 for i in range(n):
     temp = sorted(arr[i])
     if temp != check:
         ans = False
+        break
 
-print(ans)
+if ans:
+    for i in range(n):
+        temp = []
+        for j in range(n):
+            if arr[j][i] in temp:
+                ans = False
+                break
+            else:
+                temp.append(arr[j][i])
+
+if ans:
+    for i in range(n):
+        temp = []
+        for j in range(3):
+            for k in range(3):
+                if arr[s][e] in temp:
+                    ans = False
+                    break
+                else:
+                    temp.append(arr[s][e])
+                    e += 1
+            e -= 3
+            s += 1
+        s -= 3
+        e += 3
+        if e == 9:
+            s += 3
+            e = 0
+
+if ans:
+    print('YES')
+else:
+    print('NO')
