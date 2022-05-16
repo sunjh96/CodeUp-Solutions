ced093de029fb2a12a46a5ac408d6fbf9a389fe9
Jangoh Jeong <sunjh96@gmail.com>
2022-05-17 01:55:33 +0800

    22/05/17

--- Inflearn/파이썬 알고리즘 문제풀이(코딩테스트 대비)/이분 탐색(결정알고리즘)&그리디 알고리즘/이분검색/Solution.py
+++ Inflearn/파이썬 알고리즘 문제풀이(코딩테스트 대비)/이분 탐색(결정알고리즘)&그리디 알고리즘/이분검색/Solution.py
@@ -1,4 +1,18 @@
 n , m = map(int, input().split())
 arr = list(map(int, input().split()))
 
-print(arr)
+arr.sort()
+start = 0
+end = n - 1
+
+while(True):
+    mid = (start + end) / 2
+    if arr[mid] == m:
+        ans = mid
+        break
+    elif arr[mid] < m:
+        start = mid + 1
+    else:
+        end = mid - 1
+
+print(ans + 1)
