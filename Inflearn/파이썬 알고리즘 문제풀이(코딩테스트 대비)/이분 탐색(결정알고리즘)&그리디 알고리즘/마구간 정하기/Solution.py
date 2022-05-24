8dc27adec8742eeb7e4d290931b37f762f55c39c
Jangoh Jeong <sunjh96@gmail.com>
2022-05-25 05:40:26 +0800

    22/05/25

--- Inflearn/파이썬 알고리즘 문제풀이(코딩테스트 대비)/이분 탐색(결정알고리즘)&그리디 알고리즘/마구간 정하기/Solution.py
+++ Inflearn/파이썬 알고리즘 문제풀이(코딩테스트 대비)/이분 탐색(결정알고리즘)&그리디 알고리즘/마구간 정하기/Solution.py
@@ -1,7 +1,32 @@
+def check_ans(arr, num):
+
+    cnt = 0
+
+    temp = arr[0]
+    for i in range(1, len(arr)):
+        if num <= arr[i] - temp:
+            temp = arr[i]
+            cnt += 1
+
+    return cnt + 1
+
 n, k = map(int, input().split())
 arr = [int(input()) for _ in range(n)]
 
 arr.sort()
 
-start = arr[0]
-finish = arr[n - 1]
+start = 1
+finish = arr[-1]
+ans = 0
+
+while(start <= finish):
+    mid = (start + finish) // 2
+    res = check_ans(arr, mid)
+
+    if res < k:
+        finish = mid - 1
+    else:
+        ans = mid
+        start = mid + 1
+
+print(ans)
