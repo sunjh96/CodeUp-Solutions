950f99b092647d1db66af53bcb2da11f4ff6051c
Jangoh Jeong <sunjh96@gmail.com>
2022-05-27 19:51:52 +0800

    22/05/27

--- Inflearn/파이썬 알고리즘 문제풀이(코딩테스트 대비)/이분 탐색(결정알고리즘)&그리디 알고리즘/씨름선수/Solution.py
+++ Inflearn/파이썬 알고리즘 문제풀이(코딩테스트 대비)/이분 탐색(결정알고리즘)&그리디 알고리즘/씨름선수/Solution.py
@@ -3,6 +3,13 @@ import sys
 n = int(sys.stdin.readline())
 arr = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
 
+cnt = 0
+largest = 0
 arr.sort(key=lambda x: (x[0], x[1]), reverse=True)
 
-print(arr)
+for x, y in arr:
+    if largest < y:
+        largest = y
+        cnt += 1
+
+print(cnt)
