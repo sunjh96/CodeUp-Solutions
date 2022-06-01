0cbafddae763b31e1bae0d582a6bab2b7197e655
Jangoh Jeong <sunjh96@gmail.com>
2022-06-01 21:25:15 +0900

    22/06/01

--- Data Structure/Stack.py
+++ Data Structure/Stack.py
@@ -1,6 +1,31 @@
 class Stack:
     def __init__(self):
         self.items = []
+
+    def push(self, val):
+        self.items.append(val)
+
+    def pop(self):
+        try:
+            return self.items.pop()
+        except IndexError:
+            print("Stack is empty")
+
+    def top(self):
+        try:
+            return self.items[-1]
+        except IndexError:
+            print("Stack is empty")
+
+    def __len__(self):
+        return len(self.items)
+
+# 최솟값 출력 가능
+
+
+class Stack:
+    def __init__(self):
+        self.items = []
         self.min_items = []
 
     def push(self, val):
