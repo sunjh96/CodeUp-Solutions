3e532cd579a8e7a13e3d9a2fa6064089d7ebda7f
Jangoh Jeong <sunjh96@gmail.com>
2022-06-01 16:37:27 +0900

    22/06/01

--- Inflearn/파이썬 알고리즘 문제풀이(코딩테스트 대비)/이분 탐색(결정알고리즘)&그리디 알고리즘/역수열/Solution.py
+++ Inflearn/파이썬 알고리즘 문제풀이(코딩테스트 대비)/이분 탐색(결정알고리즘)&그리디 알고리즘/역수열/Solution.py
@@ -1,1 +1,18 @@
-n =
+import sys
+
+n = int(sys.stdin.readline())
+seq = list(map(int, sys.stdin.readline().split()))
+
+ans = [n for _ in range(n)]
+
+for i in range(1, n + 1):
+    cnt = 0
+    k = seq[i - 1]
+    for j in range(n):
+        if i < ans[j]:
+            cnt += 1
+        if cnt == k + 1:
+            ans[j] = i
+            break
+
+print(*ans)
