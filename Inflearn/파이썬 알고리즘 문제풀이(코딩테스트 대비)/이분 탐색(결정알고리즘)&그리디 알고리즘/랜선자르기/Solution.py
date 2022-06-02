1e4e1466655551bdf40973e6e965d6ce0056a85b
Jangoh Jeong <sunjh96@gmail.com>
2022-06-03 08:58:12 +0900

    22/06/03

--- Inflearn/파이썬 알고리즘 문제풀이(코딩테스트 대비)/이분 탐색(결정알고리즘)&그리디 알고리즘/랜선자르기/Solution.py
+++ Inflearn/파이썬 알고리즘 문제풀이(코딩테스트 대비)/이분 탐색(결정알고리즘)&그리디 알고리즘/랜선자르기/Solution.py
@@ -5,6 +5,7 @@ def check_ans(mid):
 
     return cnt
 
+
 n, m = map(int, input().split())
 arr = []
 
@@ -19,14 +20,11 @@ while(True):
     mid = (first + last) // 2
     cnt = check_ans(mid)
 
-    if last < first:
+    if last <= first:
         break
     if cnt >= m:
         ans = mid
         first = mid + 1
     elif cnt < m:
         last = mid - 1
-    else:
-        first = mid + 1
-
 print(ans)
