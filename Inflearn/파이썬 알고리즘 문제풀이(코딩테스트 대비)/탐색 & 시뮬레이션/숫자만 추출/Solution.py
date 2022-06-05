d787cc37cc7585a2bb81eefb5135323d88215d8d
Jangoh Jeong <sunjh96@gmail.com>
2022-06-05 22:40:18 +0900

    22/05/13

--- Inflearn/파이썬 알고리즘 문제풀이(코딩테스트 대비)/탐색 & 시뮬레이션/숫자만 추출/Solution.py
+++ Inflearn/파이썬 알고리즘 문제풀이(코딩테스트 대비)/탐색 & 시뮬레이션/숫자만 추출/Solution.py
@@ -7,11 +7,12 @@ for i in string:
     if i.isdecimal():
         ans = 10 * ans + int(i)
 
-for i in range(1, ans //2 + 1):
+for i in range(1, ans // 2 + 1):
     if ans % i == 0:
         cnt += 1
 
 print(ans, cnt + 1)
+# isalpha, isdecimal, isupper, islower
 '''
 for i in string:
     if i.isalpha() == False:
