9a19a15fba38810d9d35003d78d63a9cd5c5b18f
Jangoh Jeong <sunjh96@gmail.com>
2022-07-22 00:43:00 +0900

    22/07/22

--- BOJ/SILVER V/1343-폴리오미노.py
+++ BOJ/SILVER V/1343-폴리오미노.py
@@ -2,9 +2,28 @@ import sys
 input = sys.stdin.readline
 
 
-def sol():
-    pass
+def sol(board):
+    cnt = 0
+
+    for idx, val in enumerate(board):
+        if val == 'X':
+            cnt += 1
+        else:
+            if cnt == 0:
+                continue
+            else:
+                if cnt % 2 == 0:
+                    board = board[:idx].replace(
+                        'X', 'A', 4*(cnt//4)) + board[idx:]
+                    board = board[:idx].replace('X', 'B') + board[idx:]
+                    cnt = 0
+                else:
+                    return -1
+
+    return board
 
 
 if __name__ == '__main__':
-    pass
+    board = input()
+
+    print(sol(board))
