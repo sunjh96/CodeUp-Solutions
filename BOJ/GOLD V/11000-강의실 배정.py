64c8ec80ad849a3a4be105a6786360efdb2cd392
Jangoh Jeong <sunjh96@gmail.com>
2022-07-21 23:01:49 +0900

    22/07/21

--- BOJ/GOLD V/11000-강의실 배정.py
+++ BOJ/GOLD V/11000-강의실 배정.py
@@ -4,22 +4,37 @@ input = sys.stdin.readline
 
 def sol():
     cnt = 0
+    class_time.sort(key=lambda x: (x[1], x[0]))
+    end = class_time[0][1]
+
     while(True):
-        if not class_time:
-            return cnt
-        class_time.sort(key=lambda x: (x[1], x[0]))
 
-        end = class_time[0][1]
-
-        for x, y in class_time[1:]:
+        del class_time[0]
+        for idx, (x, y) in enumerate(class_time):
             if end <= x:
                 end = y
-                del class_time[class_time.index([x, y])]
+                del class_time[idx]
+
         cnt += 1
 
+        if len(class_time) <= 1:
+            return cnt
+
+        end = class_time[0][1]
 
 if __name__ == '__main__':
     n = int(input())
     class_time = [list(map(int, input().split())) for _ in range(n)]
 
-    print(sol())
+    if n == 1:
+        print(1)
+    else:
+        print(sol())
+
+
+# 1 3
+# 2 4
+# 3 5
+# 6 9
+# 7 8
+# 9 10
---
+++ BOJ/SILVER V/1343-폴리오미노.py
@@ -0,0 +1,10 @@
+import sys
+input = sys.stdin.readline
+
+
+def sol():
+    pass
+
+
+if __name__ == '__main__':
+    pass
